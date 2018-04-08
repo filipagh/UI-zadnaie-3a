e2e73788245c2f367e7475c4445450b38ead7519
Pumec <Filip11agh@gmail.com>
2018-04-09 00:56:34 +0200

    zacatie fittnes f.

--- src/ai/Stav.java
+++ src/ai/Stav.java
@@ -3,9 +3,103 @@ package ai;
 public class Stav {
 	
 	int [][] pole;
-	public stav()
+	int [] geny;
+	int x;
+	int y;
+	int smerx;
+	int smery;
+	public Stav(int [][] zapis,int[] geny_imp)
 	{
+		pole=zapis.clone();
+		geny=geny_imp.clone();
+		for (int i=0;i<28;i++)
+		{
+			System.out.print(geny[i]+" ");
+		
+		}
+		System.out.println();
+		presun();
+	}
+	
+	
+	public void presun()
+	{
+		for (int i=1;i<28;i++)
+		{
+		
+				do
+				{
+					if (geny[i]<13)
+					{
+						x=(geny[i]-1);
+						y=0;
+						smerx =0;
+						smery =1;
+
+						break;
+					}
+					else if (geny[i]<23)
+					{
+						x=11;
+						y=geny[i]-13;
+						smerx =-1;
+						smery =0;
+						break;
+					}
+					if (geny[i]<35)
+					{
+						x=(geny[i]-34)*-1;
+						y=9;
+						smerx =0;
+						smery =-1;
+						break;
+					}
+					else 
+					{
+						x=0;						
+						y=(geny[i]-44)*-1;
+						smerx =1;
+						smery =0;
+						break;
+					}
+					
+				}while (true);
+				
+				hrabaj(i);
+		
+		}
 		
 	}
+
+	public void hrabaj(int i)
+	{
+		if (pole[y][x]!= 0)	
+		{
+			System.out.println(x+" kamen "+y);
+			return;	
+		}
+
+		while (true)
+		{
+			if ((y+smery>=0 && y+smery<10) && (x+smerx>=0 && x+smerx<12) && (pole[y+smery][x+smerx]==0))
+			{
+				pole[y][x]=i;
+				y+=smery;
+				x+=smerx;
+			
+			}
+			else
+			{
+				if ((y+smerx>=0 && y+smerx<10) && (x+smery>=0 && x+smery<12) && (pole[y][x])  )
+				{
+					
+				}
+			}
+			
+		}
+		
+		
+	}
+
 	
 }
--- src/ai/Ui.java
+++ src/ai/Ui.java
@@ -1,17 +1,61 @@
 package ai;
 
+import java.util.Random;
+
 public class Ui {
 
-	static int [][] pole;
+	public static int [][] zapis;
 	
 	  public static void main (String[] args)
 	   	{
 		System.out.println("test");
  
-		 pole=new int[10][12];
+		 zapis=new int[10][12];
 		 napln();
-      
+     
+		 vytvor();
+		
 	}
+	
+	public static void vytvor()
+	{
+		int[] geny =new int[45];
+		int[] geny_exp = new int[28];
+	    for (int i=1;i<45;i++)
+	    {
+	    	geny[i]=i;
+	    }
+	    Random rand =new Random();
+	    for (int i=0;i<200;i++)
+	    {
+	    	int a =rand.nextInt(44)+1;
+	    	int b =rand.nextInt(44)+1;
+	    	int c;
+	    	c=geny[a];
+	    	geny[a]=geny[b];
+	    	geny[b]=c;
+	    }
+	    
+	    
+	    geny_exp=new int[28];
+	    
+	    for (int i=1;i<28;i++)
+	    {
+	    	geny_exp[i]=geny[i];
+	    }
+	  
+	   
+
+	    
+		Stav a = new Stav(zapis,geny_exp); 
+	}
+	  
+	  
+	  
+	  
+	
+	
+	
 	
 	public static void napln()
 	{
@@ -20,27 +64,33 @@ public class Ui {
 		{
 			for (int k=0;k<12;k++)
 			{
-				pole[i][k]=0;
+				zapis[i][k]=0;
 				
 			}
 			
 		}
-	pole[1][5]=-1;
-	pole[2][1]=-1;
-	pole[3][4]=-1;
-	pole[4][2]=-1;
-	pole[6][8]=-1;
-	pole[6][9]=-1;
+	zapis[1][5]=-1;
+	
+	zapis[0][1]=-1;
+	zapis[0][2]=-1;
+	zapis[0][3]=-1;
+	zapis[0][4]=-1;
+	zapis[0][5]=-1;
+	zapis[0][6]=-1;
+	
+	zapis[2][1]=-1;
+	zapis[3][4]=-1;
+	zapis[4][2]=-1;
+	zapis[6][8]=-1;
+	zapis[6][9]=-1;
 		for (int i=0;i<10;i++ )
 		{
 			for (int k=0;k<12;k++)
 			{
-				System.out.print(pole[i][k]+", ");
+				System.out.print(zapis[i][k]+", ");
 			}
 			System.out.println();
 		}
-	
-
 	}
 	
 	
