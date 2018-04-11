de958b25aaf29797cafebc3ec843f32c7447ba37
Pumec <Filip11agh@gmail.com>
2018-04-11 23:26:02 +0200

    dokumentacia fin

--- src/ai/Stav.java
+++ src/ai/Stav.java
@@ -1,10 +1,10 @@
 package ai;
 
 public class Stav {
-	
-	
+
+
 	// tree map
-	
+
 	int [][] pole;
 	int [] geny;
 	int x;
@@ -14,19 +14,23 @@ public class Stav {
 	int skore;
 	public Stav(int[] geny_imp)
 	{
-		pole=new int[10][12];
-		vytvorpole();
-		geny=  geny_imp.clone();
+		pole=new int[10][12];			//pole na zapis zahrady
+		vytvorpole();					// naplni dane pole
+		geny=  geny_imp.clone();		// nacitanie genov
+
+		// vypis genov 
+		/*
 		for (int i=0;i<28;i++)
 		{
-		//	System.out.print(geny[i]+", ");
-		
+			System.out.print(geny[i]+", ");
+
 		}
-		//System.out.println();
+		System.out.println();
+		 */
 	}
 
-	
-	public boolean over_sur(int x, int y)
+
+	public boolean over_sur(int x, int y)  // metoda ktora skontroluje ci dane sur su v poly 
 	{
 		if ((y>=0 && y<10) && (x>=0 && x<12))
 		{
@@ -34,63 +38,64 @@ public class Stav {
 		}
 		return false;
 	}
-	
-	public void presun()
-	{
-		int pocet_ciest=0;
-		for (int i=1;i<28;i++)
-		{
-		
-				do
-				{
-					if (geny[i]<13)
-					{
-						x=(geny[i]-1);
-						y=0;
-						smerx =0;
-						smery =1;
 
-						break;
-					}
-					else if (geny[i]<23)
-					{
-						x=11;
-						y=geny[i]-13;
-						smerx =-1;
-						smery =0;
-						break;
-					}
-					if (geny[i]<35)
-					{
-						x=(geny[i]-34)*-1;
-						y=9;
-						smerx =0;
-						smery =-1;
-						break;
-					}
-					else 
-					{
-						x=0;						
-						y=(geny[i]-44)*-1;
-						smerx =1;
-						smery =0;
-						break;
-					}
-					
-				}while (true);
-				//System.out.println(x+" ------------------- "+y);
-				if (pole[y][x]!= 0)	
+	public void presun() 			// metoda na pocitenie FITNES 
+	{
+		int pocet_ciest=0;     //pocet ciest
+		for (int i=1;i<28;i++)  // pre kazdy gen zacni hladat cestu
+		{
+
+			do
+			{
+				if (geny[i]<13)				// nastavenie smeru podla umiestnenia v poly
 				{
-					//System.out.println(x+" kamen "+y);
+					x=(geny[i]-1);
+					y=0;
+					smerx =0;
+					smery =1;
+
+					break;
 				}
-				else
+				else if (geny[i]<23)
 				{
-					pocet_ciest++;
-				
-					hrabaj(pocet_ciest);
+					x=11;
+					y=geny[i]-13;
+					smerx =-1;
+					smery =0;
+					break;
 				}
+				if (geny[i]<35)
+				{
+					x=(geny[i]-34)*-1;
+					y=9;
+					smerx =0;
+					smery =-1;
+					break;
+				}
+				else 
+				{
+					x=0;						
+					y=(geny[i]-44)*-1;
+					smerx =1;
+					smery =0;
+					break;
+				}
+
+			}while (true);
+			
+			
+			if (pole[y][x]!= 0)	 // test ci mozem vstupit na dane policko // ci tam nieje kamen alebo ina cesta
+			{
+				//System.out.println(x+" "+y+" je zablokovana");
+			}
+			else
+			{
+				pocet_ciest++;			// zvys pocet ciest a zacni ju prehladavat
+
+				hrabaj(pocet_ciest);
+			}
 		}
-	
+								// vypocet fitnes skore ... poscitam pocet 0 v zahrade
 		skore = 10*12;
 		for (int p=0;p<10;p++ )
 		{
@@ -98,14 +103,14 @@ public class Stav {
 			{
 				if (pole[p][k]==0)
 				{
-				 skore--;	
+					skore--;	
 				}
 			}
 		}
 		//System.out.println(skore);
 	}
 
-	public void zobraz()
+	public void zobraz()		// metoda na vypisanie zahrady
 	{
 		for (int p=0;p<10;p++ )
 		{
@@ -116,24 +121,24 @@ public class Stav {
 			System.out.println();
 		}
 	}
-	
-	public void hrabaj(int i)
+
+	public void hrabaj(int i)     // metoda prechadzania zahradov PREDPOKLAD nachadzame sa na kraji zahrady 
 	{
-	
-		
-		pole[y][x]= i;
+
+
+	//	pole[y][x]= i; 			// zapiseme cislo na zaciatocne miesto
 		while (true)
 		{
-			if (over_sur(x+smerx,y+smery) && (pole[y+smery][x+smerx]==0))
+			if (over_sur(x+smerx,y+smery) && (pole[y+smery][x+smerx]==0)) 		// test vieme sa posunut dopredu ak ano 
 			{
 				pole[y][x]=i;
 				y+=smery;
 				x+=smerx;
-			
+
 			}
 			else
 			{
-				if (over_sur(x+smerx,y+smery)==false)
+				if (over_sur(x+smerx,y+smery)==false) // zisti ci niesi na konci tahu //    start  1111111111<>  koniec
 				{
 					pole[y][x]=i;
 					break;
@@ -160,22 +165,28 @@ public class Stav {
 					} 
 					*/ 
 				}
-				else
+				else  // inac nastala situacia ze som do niecoho vrazil 
 				{
-
-					if ((over_sur(x+smery,y+smerx)) && (pole[y+smerx][x+smery]==0) && (over_sur(x+smery*2,y+smerx*2)) && (pole[y+smerx*2][x+smery*2]!=0))
+					/*
+				111111111
+				    5-3 
+					5-
+					5-
+						pozrieme sa v opacnom smere t.j. vlavo pravo (zamena smerx a smery) ci je jedno volne miest a za nim je obsadene (mame tah 5 narazili sme na 1 a porieme sa vpravo a vidime ze je tam volne a obsadene (3) ideme touto cestov)
+					*/
+					if ((over_sur(x+smery,y+smerx)) && (pole[y+smerx][x+smery]==0) && (over_sur(x+smery*2,y+smerx*2)) && (pole[y+smerx*2][x+smery*2]!=0))     
 					{
 						int temp=smery;
 						smery=smerx;
 						smerx=temp;
 					}
-					else if ((over_sur(x+smery*-1,y+smerx*-1)) && (pole[y+smerx*-1][x+smery*-1]==0) )
+					else if ((over_sur(x+smery*-1,y+smerx*-1)) && (pole[y+smerx*-1][x+smery*-1]==0) ) // inak poreme sa ci sa da ist opacne ak ano ideme tade
 					{
 						int temp=smery*-1;
 						smery=smerx*-1;
 						smerx=temp;
 					}
-					else if ((over_sur(x+smery,y+smerx)) && (pole[y+smerx][x+smery]==0))
+					else if ((over_sur(x+smery,y+smerx)) && (pole[y+smerx][x+smery]==0))   // inak chod doprava ak sa da 
 					{
 						int temp=smery;
 						smery=smerx;
@@ -183,9 +194,9 @@ public class Stav {
 					}
 					else
 					{
-						if ((over_sur(x+smery,y+smerx)==false) || (over_sur(x+smery*-1,y+smerx*-1)==false))
+						if ((over_sur(x+smery,y+smerx)==false) || (over_sur(x+smery*-1,y+smerx*-1)==false))  // inac over ci si na kraji (situacia ci si neskoncil v strede zahrady)
 						{
-						pole[y][x]=i;
+							pole[y][x]=i;
 						}
 						break;
 					}
@@ -194,11 +205,11 @@ public class Stav {
 			}
 
 		}
-		
-		
+
+
 	}
 
-	public void vytvorpole()
+	public void vytvorpole()  		// METODA NA VYPLNENIE POLA TEDA ZAHRADY	
 	{
 		for (int i=0;i<10;i++ )
 		{
@@ -206,14 +217,14 @@ public class Stav {
 			{
 				pole[i][k]=0;
 			}
-			
+
 		}
-	pole[1][5]=-1;	
-	pole[2][1]=-1;
-	pole[3][4]=-1;
-	pole[4][2]=-1;
-	pole[6][8]=-1;
-	pole[6][9]=-1;
+		pole[1][5]=-1;	
+		pole[2][1]=-1;
+		pole[3][4]=-1;
+		pole[4][2]=-1;
+		pole[6][8]=-1;
+		pole[6][9]=-1;
 	}
-	
+
 }
--- src/ai/Ui.java
+++ src/ai/Ui.java
@@ -10,83 +10,83 @@ import java.util.TreeMap;
 
 public class Ui {
 
-	
+
 	public static List<Stav> akt_generacia; 
 	public static List<Stav> nova_generacia;
 	public static TreeMap<Integer, Stav> tmap;
 	public static int populacia; 
-	
-	  public static void main (String[] args)
-	   	{
-		  populacia = 500;		//urcuje pocet jedincov 
-		  tmap = new TreeMap<Integer, Stav>();		// 	datova struktura pre "ruletu"			 
-		 akt_generacia = new ArrayList<Stav>();		// list jedincov aktualnej gen				
-		 
-		 int generacia=1;	//ciselnik gen
-		 vytvor(populacia); //metoda na vytvorenie 1. generacie  
-		 int sum=0; //celkova hodnota pre ruletu
 
-		 int max_skore=0;  // max hodnota fitnes f. 
-		 do
-		 {
-			 
-			 if (max_skore!=0)  // generovanie dalsej generacie
-			 {
+	public static void main (String[] args)
+	{
+		populacia = 20;		//urcuje pocet jedincov 
+		tmap = new TreeMap<Integer, Stav>();		// 	datova struktura pre "ruletu"			 
+		akt_generacia = new ArrayList<Stav>();		// list jedincov aktualnej gen				
+
+		int generacia=1;	//ciselnik gen
+		vytvor(populacia); //metoda na vytvorenie 1. generacie  
+		int sum=0; //celkova hodnota pre ruletu
+
+		int max_skore=0;  // max hodnota fitnes f. 
+		do
+		{
+
+			if (max_skore!=0)  // generovanie dalsej generacie
+			{
 				generacia++;    // zvys pocitadlo
-				
-				if (generacia==100000)   // limit 
+
+				if (generacia==10000)   // limit 
 				{
 					System.out.println("nenaslo sa riesenie do 10000 generacii");
 					return;
 				}
-				
-				
-				  nova_generacia =new ArrayList<Stav>();	//list jedincov novej generacie  
-				 for (int i=0;i < populacia;i++)			// generovanie novych jednincov
-				 {
-					 Stav prvy=null;  // otec
-					 Stav druhy=null; // matka
-					
-
-					 Random rand =new Random();
-					
-
-					 if (generacia % 2 == 0)  // RULETA
-					 {
-						 int otec = rand.nextInt(sum);   // nahodna hodnota pre otca
-						 int mama =  rand.nextInt(sum);  // nahodna hodnota pre mamku
-						 Set set = tmap.entrySet();		// struhtury pre vyber z rulety
-						 Iterator iterator = set.iterator();
-						 while(iterator.hasNext())     // Hladanie danych objektov ktore boly v rulete vybrate
-						 {
-							 Map.Entry mentry = (Map.Entry)iterator.next();
-							 if ((otec < (int) mentry.getKey()) && (prvy==null))
-							 {
-								 prvy = (Stav) mentry.getValue();
-							 }
-							 if ((mama < (int) mentry.getKey())&&(druhy==null))
-							 {
-								 druhy = (Stav) mentry.getValue();
-							 }
-
-							 if ((prvy!=null)&&(druhy!=null))
-							 {
-								 break;
-							 }
 
 
-						 }
-					 }
-					 else    // TURNAJ
-					 {
-						 //System.out.println("test 2gne alg"); 
-						  List<Stav> otcovia = new ArrayList<Stav>();  // list otcov
-						  List<Stav> matky = new ArrayList<Stav>();    // list matiek
+				nova_generacia =new ArrayList<Stav>();	//list jedincov novej generacie  
+				for (int i=0;i < populacia;i++)			// generovanie novych jednincov
+				{
+					Stav prvy=null;  // otec
+					Stav druhy=null; // matka
+
+
+					Random rand =new Random();
+
+
+					if (generacia % 2 == 0)  // RULETA
+					{
+						int otec = rand.nextInt(sum);   // nahodna hodnota pre otca
+						int mama =  rand.nextInt(sum);  // nahodna hodnota pre mamku
+						Set set = tmap.entrySet();		// struhtury pre vyber z rulety
+						Iterator iterator = set.iterator();
+						while(iterator.hasNext())     // Hladanie danych objektov ktore boly v rulete vybrate
+						{
+							Map.Entry mentry = (Map.Entry)iterator.next();
+							if ((otec < (int) mentry.getKey()) && (prvy==null))
+							{
+								prvy = (Stav) mentry.getValue();
+							}
+							if ((mama < (int) mentry.getKey())&&(druhy==null))
+							{
+								druhy = (Stav) mentry.getValue();
+							}
+
+							if ((prvy!=null)&&(druhy!=null))
+							{
+								break;
+							}
+
+
+						}
+					}
+					else    // TURNAJ
+					{
+						//System.out.println("test 2gne alg"); 
+						List<Stav> otcovia = new ArrayList<Stav>();  // list otcov
+						List<Stav> matky = new ArrayList<Stav>();    // list matiek
 						for (int p=0;p<(int) (akt_generacia.size() / 5);p++)   // vyber 20% z aktualnej generacie do otcov a do matiek
 						{
-						//	  Random rand =new Random();
-							 otcovia.add(akt_generacia.get( rand.nextInt(akt_generacia.size()))) ;
-							 matky.add(akt_generacia.get( rand.nextInt(akt_generacia.size()))) ;	
+							//	  Random rand =new Random();
+							otcovia.add(akt_generacia.get( rand.nextInt(akt_generacia.size()))) ;
+							matky.add(akt_generacia.get( rand.nextInt(akt_generacia.size()))) ;	
 						}
 
 						int najotec=0;
@@ -104,89 +104,86 @@ public class Ui {
 								najmatka = matky.get(p).skore;
 							}
 						}
-						
-					 }
-					 vloznew(urobpotomka(prvy,druhy,10,80));     // vytvor potomka(otec,mama,kolko % mutacia,kolko & krizenie) a nasledne ho vloz do zoznamu nova gen
-				 }
-				 akt_generacia=nova_generacia;   // na konci generovania nova gen sa stava sucastna
-				 tmap.clear(); // vycistenie rulety
-			 }
-		 
-						 
-	     max_skore=0; // reset max skore pre gen , aj sum pre ruletu
-	     sum=0; 
-	  		 Stav naj=null; // naj sluzi len na zobrazenie naj jedinca v gen
-		 for(Stav n : akt_generacia) // prechadzaj generaciu
-		 {
-			 n.presun(); // kazdy jedinec si urobi fitnes funkciu
-			 sum+=n.skore; // jej skore sa pripocita do sum rulety
-			 vlozdostromu(n,sum); // a vlozi sa do stromu rulety
-			 if (n.skore>max_skore) // ak je jedinec lepsi ako predosli zapise sa do "naj"
-			 {
-				 max_skore=n.skore;
-				 naj=n;	 
-			 }
-		 }
-		System.out.println("najlepsi z tejto generacie ma skore "+max_skore);   //vypis naj jedinca z gen
-		 naj.zobraz(); // zobrazi jeho riesenie
-		 } while (max_skore != 120);   // opakuj kym nemame sprvne riesenie
-		 System.out.println("generacia cislo "+generacia);  
+
+					}
+					vloznew(urobpotomka(prvy,druhy,10,80));     // vytvor potomka(otec,mama,kolko % mutacia,kolko & krizenie) a nasledne ho vloz do zoznamu nova gen
+				}
+				akt_generacia=nova_generacia;   // na konci generovania nova gen sa stava sucastna
+				tmap.clear(); // vycistenie rulety
+			}
+
+
+			max_skore=0; // reset max skore pre gen , aj sum pre ruletu
+			sum=0; 
+			Stav naj=null; // naj sluzi len na zobrazenie naj jedinca v gen
+			for(Stav n : akt_generacia) // prechadzaj generaciu
+			{
+				n.presun(); // kazdy jedinec si urobi fitnes funkciu
+				sum+=n.skore; // jej skore sa pripocita do sum rulety
+				vlozdostromu(n,sum); // a vlozi sa do stromu rulety
+				if (n.skore>max_skore) // ak je jedinec lepsi ako predosli zapise sa do "naj"
+				{
+					max_skore=n.skore;
+					naj=n;	 
+				}
+			}
+			System.out.println("najlepsi z tejto generacie ma skore "+max_skore);   //vypis naj jedinca z gen
+			
+			naj.zobraz(); // zobrazi jeho riesenie
+		} while (max_skore != 120);   // opakuj kym nemame sprvne riesenie
+		System.out.println("generacia cislo "+generacia);  
 	}
-	
-	  public static Stav urobpotomka(Stav otec,Stav mama,int mutacia,int krizenie)  // generacia na vytvaranie potomka (otec, matka, % kolko mutacie,%kolko krizenie)
+
+	public static Stav urobpotomka(Stav otec,Stav mama,int mutacia,int krizenie)  // generacia na vytvaranie potomka (otec, matka, % kolko mutacie,%kolko krizenie)
+	{
+		Random rand =new Random(); 
+		int [] geny_exp=null; 
+		if (otec.skore>mama.skore)   // ak nieje krizenie tak vlozi sa lepsi z otca/matky
 		{
-		  Random rand =new Random(); 
-		  int [] geny_exp=null; 
-		  if (otec.skore>mama.skore)   // ak nieje krizenie tak vlozi sa lepsi z otca/matky
-		  {
-			  geny_exp =  otec.geny.clone();	
-		  }
-		  else
-		  {
-			  geny_exp =  mama.geny.clone();	  
-		  }
-		  
-		  if ( rand.nextInt(100)<krizenie)			// ak je krizenie parne geny otca a neparne geny matky sa zapisu do noveho genomu
-			 {
-				  for (int i=0;i<14;i++)
-					{
-						geny_exp[i]=otec.geny[i*2];
-						geny_exp[i+14]=mama.geny[i*2+1];
-					} 
-			 }
-		  
-		
-		  
-		
-		  
-		 if ( rand.nextInt(100)<mutacia)   // ak nastane mutacia tak nahodne miesto v genone sa upravi na nahodnu hodnotu vstupu
-		 {
-			 geny_exp[rand.nextInt(27)+1]=rand.nextInt(43)+1;
-		 }
-		 
-		 
-		  return new Stav(geny_exp);   // vrati novy stav 
+			geny_exp =  otec.geny.clone();	
 		}
-	  
-		    
-	  
-	public static void vlozdostromu(Stav a,int skore)
+		else
+		{
+			geny_exp =  mama.geny.clone();	  
+		}
+
+		if ( rand.nextInt(100)<krizenie)			// ak je krizenie parne geny otca a neparne geny matky sa zapisu do noveho genomu
+		{
+			for (int i=0;i<14;i++)
+			{
+				geny_exp[i]=otec.geny[i*2];
+				geny_exp[i+14]=mama.geny[i*2+1];
+			} 
+		}
+
+		if ( rand.nextInt(100)<mutacia)   // ak nastane mutacia tak nahodne miesto v genone sa upravi na nahodnu hodnotu vstupu
+		{
+			geny_exp[rand.nextInt(27)+1]=rand.nextInt(43)+1;
+		}
+
+
+		return new Stav(geny_exp);   // vrati novy stav 
+	}
+
+
+
+	public static void vlozdostromu(Stav a,int skore)  // vlozenie stavu a jeho vahy do stromu
 	{
 		tmap.put(skore,a);
 	}
-	  
-	public static void vytvor(int n)
+
+	public static void vytvor(int n)  // metoda na generovanie 1. generacie
 	{
-		for (int p=0;p<n;p++)
+		for (int p=0;p<n;p++)   // n*populacia
 		{
-			int[] geny =new int[45];
-			int[] geny_exp = new int[28];
-			for (int i=1;i<45;i++)
+			int[] geny =new int[45];       // pole vsetkych vstupov
+			int[] geny_exp = new int[28];  // genon
+			for (int i=1;i<45;i++)   // vytvorenie postupnosti  1 2 3 4 .... 43 44 
 			{
 				geny[i]=i;
 			}
 			Random rand =new Random();
-			for (int i=0;i<200;i++)
+			for (int i=0;i<200;i++)   // kazdy gen nahodne poprehadzujem
 			{
 				int a =rand.nextInt(44)+1;
 				int b =rand.nextInt(44)+1;
@@ -195,37 +192,30 @@ public class Ui {
 				geny[a]=geny[b];
 				geny[b]=c;
 			}
-
-
-			geny_exp=new int[28];
-
-			for (int i=1;i<28;i++)
+			for (int i=1;i<28;i++)  // prepis prvych 27 genov
 			{
 				geny_exp[i]=geny[i];
 			}
-
-			vloz(new Stav(geny_exp));
-
-			//Stav a = new Stav(zapis,geny_exp);
+			vloz(new Stav(geny_exp));    // vlozenie stavu do zoznamu akt generaciqe
 		}
 	}
-	  
-	  
-	  
+
+
+	// metody na vlozenie do zoznamu
 	public static void vloz(Stav imp)
 	{
 		akt_generacia.add(imp);
 	}
-	
+
 	public static void vloznew(Stav imp)
 	{
 		nova_generacia.add(imp);
 	}
-	
-	
-	
-	
 
-	
-	
+
+
+
+
+
+
 }
